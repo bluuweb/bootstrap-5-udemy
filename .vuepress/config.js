08c195a0776331156115b5be4d3ee73f2b1b1836
bluuweb <contacto@bluuweb.cl>
2021-01-23 11:40:05 -0300

    primera

---
+++ .vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: 'Bootstrap 5 (bluuweb)',
+  description: 'Aprende a trabajar con Bootstrap 5!',
+  base: '/bootstrap-5-udemy/',
+  locales:{
+    '/':{
+      lang: 'es-ES'
+    }
+  },
+  themeConfig:{
+    nav: [
+      { text: 'Guías', link: 'https://bluuweb.github.io/' },
+      // { text: 'Guia', link: '/docs/' },
+      { text: 'Youtube', link: 'https://youtube.com/bluuweb' },
+      { text: 'Curso Vue 3', link: 'http://curso-vue-js-udemy.bluuweb.cl' },
+      { text: 'Curso React', link: 'http://curso-react-js-udemy.bluuweb.cl' }
+    ],
+    sidebar:
+      [
+        '/',
+        // '/01-fundamentos/',
+        '/01-bases/',
+        '/02-componentes/',
+        '/10-parcel/',
+      ]
+  }
+ 
+}
+
+
+{/* <img :src="$withBase('/img/compu-1.gif')"> */}
